3450489fcfab3234593dd80d21749916c0d17381
javed.qadruddin <javed.qadruddin@gmail.com>
2013-09-27 11:02:19 -0700

    Initial Commit

---
+++ GeneticAlg/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  GeneticAlg
+//
+//  Created by Javed Qadrud-Din on 9/27/13.
+//  Copyright (c) 2013 Javed Qadrud-Din. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
